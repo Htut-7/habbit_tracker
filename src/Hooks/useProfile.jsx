ef7ef194c1c93e603ad8b7b70c49e23664efc935
Champ is God <hsithuhtut@gmail.com>
2026-07-23 15:49:11 +0700

    feat: implement profile update function and fix some css for profile page

--- src/Hooks/useProfile.jsx
+++ src/Hooks/useProfile.jsx
@@ -1,10 +1,112 @@
-// import { useState } from "react"
+import { useState } from "react"
+import { EmailAuthProvider, updateProfile as updateAuthProfile, reauthenticateWithCredential, updatePassword, updateEmail } from "firebase/auth";
+import { auth,db } from "../Firebase/firebase";
+import { updateDoc, doc } from "firebase/firestore";
 
 
-// export default function useProfile() {
+export default function useProfile() {
 
-//     const [loading,setLoading]=useState(false);
-//     const [error,setError]=useState(null);
+    const [loading,setLoading]=useState(false);
+    const [error,setError]=useState(null);
 
-//   return {loading, error }
-// }
+    const getProfile=()=>{
+        const user=auth.currentUser;
+
+        if(!user){
+            return null
+        };
+
+        return {
+            name: user.displayName || "",
+            email: user.email || "",
+        };
+    };
+
+    const updateProfile=async(name,email)=>{
+        const user=auth.currentUser;
+        
+        if(!user){
+            throw new Error('User must be Logged in');
+        }
+
+        try{
+            setLoading(true);
+            setError(null);
+
+            if(name.trim()===""){
+                setError('Name is required')
+                return false;
+            };
+
+            if(email.trim()===""){
+                setError('Email is required')
+                return false;
+            };
+
+            if(user.name !== name){
+                await updateAuthProfile(user,{
+                    displayName: name
+                })
+            };
+
+            if(user.email !== email){
+                await updateEmail(user,email)
+            };
+
+            await updateDoc(doc(db,"users",user.uid),{
+                name,
+                email,
+            });
+            return true;
+
+        }catch(error){
+            setError(error.message);
+            setLoading(false);
+            return false;
+        }finally{
+            setLoading(false);
+        }
+    };
+
+    const changePassword=async(curr,newPass)=>{
+        const user=auth.currentUser;
+
+        if(!user){
+            throw new Error('User must be logged in')
+        };
+
+        try{
+            setLoading(true);
+            setError(null);
+
+            if(curr.trim()===""){
+                setError('Current Password is required');
+                return false;
+            }
+
+            if(newPass.trim()===""){
+                setError('New Password is required');
+                return false;
+            }
+
+            const credential=EmailAuthProvider.credential(
+                user.email,
+                curr
+            );
+
+            await reauthenticateWithCredential(user,credential);
+
+            await updatePassword(user,newPass);
+            return true
+
+        }catch(error){
+            setError(error.message);
+            setLoading(false);
+            return false;
+        }finally{
+            setLoading(false);
+        }
+    }
+
+  return {loading, error, getProfile, updateProfile, changePassword }
+}
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../Css/Profile.css";
 import { FaPencil } from "react-icons/fa6";
+import useProfile from "../Hooks/useProfile";
+import { useNavigate } from "react-router-dom";
 
 export default function Profile() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [currPass, setCurrPass] = useState("");
+  const [newPass, setNewPass] = useState("");
+  const navigate = useNavigate();
 
-    const [name,setName]=useState('');
-    const [email,setEmail]=useState('');
-    const [currPass,setCurrPass]=useState('');
-    const [newPass,setNewPass]=useState('');
+  const { loading, error, getProfile, updateProfile, changePassword } =
+    useProfile();
+
+  useEffect(() => {
+    const profile = getProfile();
+
+    if (profile) {
+      setName(profile.name);
+      setEmail(profile.email);
+    }
+  }, []);
+
+  const updateUser = async (e) => {
+    e.preventDefault();
+    const profileUpdated = await updateProfile(name, email);
+    if (!profileUpdated) {
+      return;
+    }
+
+    if (currPass.trim() && newPass.trim()) {
+      await changePassword(currPass, newPass);
+    }
+    setCurrPass("");
+    setNewPass("");
+    navigate("/");
+  };
 
   return (
     <div className="profile-page">
@@ -19,34 +48,54 @@ export default function Profile() {
         </p>
 
         <div className="profile-page-form">
-            <form>
-                <label>Username</label>
-                <input type="text" placeholder="Enter usename"
-                onChange={(e)=>setName(e.target.value)} value={name}
-                />
+          <form onSubmit={updateUser}>
+            <label>Username</label>
+            <input
+              type="text"
+              placeholder="Enter usename"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
 
-                <label>Email</label>
-                <input type="email" placeholder="Enter email"
-                onChange={(e)=>setEmail(e.target.value)} value={email}
-                />
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
 
-                <label>Current Password</label>
-                <input type="password" placeholder="Enter Current Password"
-                onChange={(e)=>setCurrPass(e.target.value)} value={currPass}
-                />
+            <label>Current Password</label>
+            <input
+              type="password"
+              placeholder="Enter Current Password"
+              onChange={(e) => setCurrPass(e.target.value)}
+              value={currPass}
+            />
 
-                <label>New Password</label>
-                <input type="password" placeholder="Enter New Password"
-                onChange={(e)=>setNewPass(e.target.value)} value={newPass}
-                />
+            <label>New Password</label>
+            <input
+              type="password"
+              placeholder="Enter New Password"
+              onChange={(e) => setNewPass(e.target.value)}
+              value={newPass}
+            />
 
-                <div className="profile-form-btn">
-                    <button>
-                        <FaPencil/>
-                        Update
-                    </button>
-                </div>
-            </form>
+            {error && <p className="profile-error">{error}</p>}
+
+            <div className="profile-form-btn">
+              <button type="submit" disabled={loading}>
+                {loading ? (
+                  <span className="profile-spinner"></span>
+                ) : (
+                  <>
+                    <FaPencil />
+                    <span>Update</span>
+                  </>
+                )}
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
